09e3be2a920988655837dced0d5e63fd60d24027
cmontilla <carlos.jonikes14@gmail.com>
2025-05-13 17:08:25 -0400

    added scroll-area & table components to be used in conjuction to the new client component which has been partially implemented.

--- src/components/clients/newClient.tsx
+++ src/components/clients/newClient.tsx
@@ -3,6 +3,7 @@
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { ScrollArea } from "../ui/scroll-area"
 
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
@@ -17,14 +18,16 @@ import {
     FormField
  } from "../ui/form"
 import {
-    Card,
-    CardTitle,
-    CardHeader,
-    CardFooter,
-    CardDescription, 
-    CardContent
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
- } from "../ui/card"
+import { Separator } from "../ui/separator"
 
 const clientSchema = z.object({
     firstname: z
@@ -38,8 +41,9 @@ const clientSchema = z.object({
         .max(50,{ message: "The name must not exceed nmore than 50 characters"})
     ,
     phonenumber: z
-        .number()
-        .min(10, { message: "The Phone number must be at least 10 numbers"})
+        .string()
+        .min(10, { message: "Invalid phone number format. Example: 123-456-7890" })
+        .regex(/^\d{3}-\d{3}-\d{4}$/, {message: "Invalid phone number format. Example: 123-456-7890"})
     ,
     dateofbirth: z
         .string()
@@ -51,77 +55,257 @@ const clientSchema = z.object({
     ,
     sex: z
         .string()
+    ,
+    street: z
+        .string()
+    ,
+    city: z
+        .string()
+    ,
+    state: z
+        .string()
+    ,
+    postalcode: z
+        .string()
+    ,
+    householdsize: z
+        .string(),
 });
 
 
 export function NewClient({}) {
 
     function onSubmit() {
-        
+        console.log("")
     }
 
+    function onClear(){
+        console.log("")
+    }
 
     const form = useForm<z.infer<typeof clientSchema>>({
         resolver: zodResolver(clientSchema),
         defaultValues: {
             firstname: "",
             lastname: "",
-            phonenumber: 0,
+            phonenumber: "",
 
         },
     })
   return (
     <div className="flex items-center justify-center min-h-screen w-full">
-        <div className="w-full max-w-md px-4">
-            <Card>
-                <CardHeader>
-                    <CardTitle>
-                        <Label>Client Information</Label>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                            <div className="grid-cols-3 flex">
-                            <FormField
+        <div className="w-full max-w-xl px-1">
+            
+                <Form {...form}>
+                    <form 
+                        onSubmit={form.handleSubmit(onSubmit)} 
+                        className="space-y-8 max-w-2xl flex flex-col  mx-auto border p-6 rounded-md">
+                        <FormLabel className="flex items-center justify-center">New Client Form</FormLabel>
+                        <h2 className="text-xl">{"Client's Information"}</h2>
+                        <Separator />
+                        <div className="grid grid-cols-2 gap-4 grid-rows-1">
+                        <FormField
+                        control={form.control}
+                        name="firstname"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormLabel>First Name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="John" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Client's public display first name.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <FormField
+                        control={form.control}
+                        name="lastname"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormLabel>Last name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Doe" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Client's public display last name.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        </div>
+                        <FormField
                             control={form.control}
-                            name="firstname"
+                            name="email"
                             render={({ field }) => (
                                 <FormItem>
-                                <FormLabel>Username</FormLabel>
+                                <FormLabel>Email Address</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
+                                    <Input placeholder="JohnDoe@example.com" {...field} />
                                 </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
+                                {/* <FormDescription>
+                                    Email address.
+                                </FormDescription> */}
                                 <FormMessage />
                                 </FormItem>
                             )}
                             />
-                            <FormField
-                            control={form.control}
-                            name="firstname"
-                            render={({ field }) => (
-                                <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                                <FormMessage />
-                                </FormItem>
-                            )}
-                            />
+                            <div className="grid grid-cols-2 gap-2">
+                                <FormField
+                                control={form.control}
+                                name="phonenumber"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>Phone Number</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="856-111-2222" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        House or Cellphone Number.
+                                    </FormDescription>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                                <FormField
+                                control={form.control}
+                                name="sex"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>Sex</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Male" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Client's Sex / Gender.
+                                    </FormDescription>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
                             </div>
-                            <Button type="submit">Submit</Button>
-                        </form>
-                    </Form>
-                </CardContent>
-            </Card>
-        </div>
+                            <h2 className="text-xl">{"Address"}</h2>
+                            <Separator/>
+                            <div className="grid grid-cols-2 gap-2">
+                                <FormField
+                                control={form.control}
+                                name="street"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>Street</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="123 Sesame Street Apt #2" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                                <FormField
+                                control={form.control}
+                                name="city"
+                                render={({ field }) => (
+                                    <FormItem>
+                                    <FormLabel>City</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Newark" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                        </div>
+                        <div className="grid grid-cols-3 gap-2">
+                            {/* <FormField
+                            control={form.control}
+                            name="aStateAbbreviation"
+                            render={({ field }) => <StateAbreviationField field={field} />}
+                            /> */}
+                            <FormField
+                            control={form.control}
+                            name="state"
+                            render={({ field }) => (
+                                <FormItem>
+                                <FormLabel>State</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="0" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                                </FormItem>
+                            )}
+                            />
+                            <FormField
+                            control={form.control}
+                            name="postalcode"
+                            render={({ field }) => (
+                                <FormItem>
+                                <FormLabel>Postal Code</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="0" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                                </FormItem>
+                            )}
+                            />
+                            <FormField
+                            control={form.control}
+                            name="householdsize"
+                            render={({ field }) => (
+                                <FormItem>
+                                <FormLabel>Household Size</FormLabel>
+                                <FormControl>
+                                    <Input
+                                    type="number"
+                                    min={1}
+                                    step={1}
+                                    placeholder="1"
+                                    {...field}
+                                    onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                                </FormItem>
+                            )}
+                            />
+                        </div>
+                        <h2 className="text-xl">{"Qualifying Reasons"}</h2>
+                        <Separator/>
+                        <div>
+                            {/* Qualifyin reasons */}
+                        </div>
+                        <h2 className="text-xl">{"Household Members"}</h2>
+                        <Separator/>
+                        <div>
+                            <Table>
+                                <TableCaption>A list of your recent invoices.</TableCaption>
+                                <TableHeader>
+                                    <TableRow>
+                                    <TableHead className="w-[100px]">Invoice</TableHead>
+                                    <TableHead>Status</TableHead>
+                                    <TableHead>Method</TableHead>
+                                    <TableHead className="text-right">Amount</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow>
+                                    <TableCell className="font-medium">INV001</TableCell>
+                                    <TableCell>Paid</TableCell>
+                                    <TableCell>Credit Card</TableCell>
+                                    <TableCell className="text-right">$250.00</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </div>
+                        <div className="grid grid-cols-2 gap-20">
+                            <Button type="reset" variant="secondary" onClick={onClear}>Clear</Button>
+                            <Button type="submit"  variant="default" onClick={onSubmit}>Submit</Button>
+                        </div>
+                    </form>
+                </Form>
+          
+        </div> 
     </div>
   )
 }
