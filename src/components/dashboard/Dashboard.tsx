ab356d65a0ffcec8057582bfc9c6f77d9f333c52
CJonikes-PC <carlos.jonikes18@icloud.com>
2025-05-12 23:09:18 -0400

    added sidebar components and initial gui components.

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export function Dashboard({className,
+    ...props
+}: React.ComponentProps<"div">) {
+    return(
+        <div>
+            <h1>Dashboard Menu</h1>
+        </div>
+    )
+}
+
